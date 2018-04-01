5bec93039f83bd6499771ae784684b60ba7f27dc
Jonathan Powers <bigblackrobot@gmail.com>
2018-03-31 21:36:05 -0500

    Added a basic tagging feature for blog posts.

--- php/blog.php
+++ php/blog.php
@@ -5,23 +5,31 @@ require('global.php');
 $conn = db_connect();
 
 //Attempt to get URL parameters.
-$id = htmlspecialchars($_GET["id"]);
-$archive = htmlspecialchars($_GET["archive"]);
+if ( isset($_GET["id"]) ) { $id = htmlspecialchars($_GET["id"]); }
+if ( isset($_GET["archive"]) ) { $archive = htmlspecialchars($_GET["archive"]); }
+if ( isset($_GET["tag"]) ) { $tags = htmlspecialchars($_GET["tag"]); }
 
-//Set the blog post query.
-if ( empty($id) && empty($archive) ) {
-    //User has requested the main blog page.
+switch (true) {
+
+	case ( isset($archive) && $archive == 'true' ): //User has requested the archive blog page.
+		$sql = "SELECT * FROM blog ORDER BY timestamp desc";
+		break;
+
+	case ( isset($id) ): //User has requested a specific blog post.
+		$sql = "SELECT * FROM blog where id = " . $id;
+		break;
+
+	case ( isset($tags) ): //User has requested a specific tag page.
+		$sql = "SELECT * FROM blog where tags LIKE '%" . $tags . ";%'";
+		break;
+
+	default: //User has requested the main blog page.
     $sql = "SELECT * FROM blog ORDER BY timestamp desc LIMIT 10";
-} elseif ( $archive == 'true' )
-	//User has requested the archive blog page.
-	$sql = "SELECT * FROM blog ORDER BY timestamp desc";
-else {
-    //User has requested a specific blog post.
-    $sql = "SELECT * FROM blog where id = " . $id;
+    break;
+
 }
 
-//Execute the blog post query.
-$result_blog_post_query = $conn->query($sql);
+ $result_blog_post_query = $conn->query($sql);
 
 //Set the last five films watched query.
 $sql = "SELECT * FROM film WHERE backdated <> \"Yes\" ORDER BY timestamp desc LIMIT 5";
@@ -52,85 +60,107 @@ $result_last_five_films_watched_query = $conn->query($sql);
 			<!-- Get blog posts. -->
 			<?php
 
-				//User has requested the main blog page.
-				if ( empty($archive) or $archive != 'true' ) {
+				switch (true) {
 
-					while($row = $result_blog_post_query->fetch_assoc()) {
-					    
-					  echo "<div class=\"blog_post_container\">";
+					case ( isset($archive) && $archive == 'true' ): //User has requested the archive blog page.
+						
+						echo "<p class=\"archive_header\">The Blog Archive</p>";
 
-					  //Format the blog post's timestamp.
-						$epoch = $row["timestamp"];
-					  $dt = new DateTime("@$epoch");
+						//Print a list of all full blog posts.
+						echo "<ul>";
 
-					  //Handle a full blog post.
-					  if ( $row["is_mini"] == "false" ) {
+						while($row = $result_blog_post_query->fetch_assoc()) {
 
-					  	//Print the blog post's header.
-					  	echo "<p class=\"blog_post_header\">" . $row["title"] . "</p>";
+							//Format the blog post timestamp.
+							$epoch = $row["timestamp"];
+						  $dt = new DateTime("@$epoch");	
+						    					  
+						  if ( $row["is_mini"] == "false" ) {
 
-					  	//Print the blog post's timestamp/perm link.
-					  	echo "<p class=\"blog_post_timestamp\"><i class=\"fa fa-clock-o\" aria-hidden=\"true\" style=\"color: #F06768; font-size: 12px;\"></i> <a href=\"blog.php?id=" . $row["id"] . "\">Blog Post from " . $dt->format('F j, Y') . "</a></p>";
+						  	echo "<li class=\"archive_list\"><a href=\"blog.php?id=" . $row["id"] . "\">" . $row["title"] . "</a> <span class=\"archive_list_timestamp\">(" . $dt->format('n/j/y') . ")</span></li>";
 
-					  	//Print the blog post's body.
-					  echo htmlspecialchars_decode($row["body"]);
+							}
 
-					  }					  										    					  					  
+						}							
 
-					  //Handle a mini blog post.
-					  if ( $row["is_mini"] == "true" ) {					  	
+						echo "</ul>";
 
-					  	//Print the blog post's body.
-					  	echo "<p>" . htmlspecialchars_decode($row["body"]) . "</p>";
+						echo "<p class=\"navigation_link\"><a href=\"blog.php\"><i class=\"fa fa-arrow-circle-o-left\" aria-hidden=\"true\" style=\"font-size: 14px;\"></i> Back to homepage...</a></p>";
 
-					  	//Print the blog post's timestamp/perm link.
-					  	echo "<p class=\"blog_post_timestamp\"><i class=\"fa fa-clock-o\" aria-hidden=\"true\" style=\"color: #F06768; font-size: 12px;\"></i> <a href=\"blog.php?id=" . $row["id"] . "\">Mini Post from " . $dt->format('F j, Y') . "</a></p>";
+						break;
 
-					  }
+					default: //User has NOT requested the archive blog page.
+				  
+						while($row = $result_blog_post_query->fetch_assoc()) {
+						    
+						  echo "<div class=\"blog_post_container\">";
 
-					  echo "</div>";
+						  //Format the blog post's timestamp.
+							$epoch = $row["timestamp"];
+						  $dt = new DateTime("@$epoch");
 
-					}
+						  //Handle a full blog post.
+						  if ( $row["is_mini"] == "false" ) {
 
-					//If this is not a perm link page, provide a link to the blog archive. Otherwise, provide a link back to the homepage.
-					if ( empty($id) ) {
+						  	//Print the blog post's header.
+						  	echo "<p class=\"blog_post_header\">" . $row["title"] . "</p>";
 
-						echo "<p class=\"navigation_link\"><i class=\"fa fa-arrow-circle-o-right\" style=\"color: #F06768; font-size: 14px;\" aria-hidden=\"true\"></i><a href=\"blog.php?archive=true\"> More blog posts...</a></p>";
+						  	//Extract the blog post's tags.
+						  	$tags_as_links = "";
+						  	if ( !empty($row["tags"]) ) {
 
-					} else {
+				  		  	$tags_array = explode( ";", $row["tags"] );				  		  	
+				  		  	foreach ($tags_array as &$tag) {
 
-						echo "<p class=\"navigation_link\"><a href=\"blog.php\"><i class=\"fa fa-arrow-circle-o-left\" aria-hidden=\"true\" style=\"font-size: 14px; \"></i> Back to homepage...</a></p>";
+				  					$new_tag = "<a href=\"blog.php?tag=" . $tag . "\"><i class=\"fas fa-tag\"></i> " . $tag . "</a>&nbsp;&nbsp;";
 
-					}				
+				  					$tags_as_links = $tags_as_links . $new_tag;
 
-				}
-				//User has requested the archive blog page.
-				else {
+				  				}
+				  				
+				  				//Clean up the tags array, by ensuring that a blank tag doesn't get added to the end and also adding various formatting to it.
+				  				$tags_as_links = str_replace("<a href=\"blog.php?tag=\"><i class=\"fas fa-tag\"></i> </a>&nbsp;&nbsp;","",$tags_as_links);
+				  				$tags_as_links = "&nbsp;&nbsp;<span class=\"tags\">" . $tags_as_links . "</span>";
 
-					echo "<p class=\"archive_header\">The Blog Archive</p>";
+						  	}						  	
 
-					//Print a list of all full blog posts.
-					echo "<ul>";
+						  	//Print the blog post's timestamp/perm link.
+						  	echo "<p class=\"blog_post_timestamp\"><i class=\"fa fa-clock-o\" aria-hidden=\"true\" style=\"color: #F06768; font-size: 12px;\"></i> <a href=\"blog.php?id=" . $row["id"] . "\">Blog Post from " . $dt->format('F j, Y') . "</a>$tags_as_links</p>";
 
-					while($row = $result_blog_post_query->fetch_assoc()) {
+						  	//Print the blog post's body.
+						  	echo htmlspecialchars_decode($row["body"]);
 
-						//Format the blog post timestamp.
-						$epoch = $row["timestamp"];
-					  $dt = new DateTime("@$epoch");	
-					    					  
-					  if ( $row["is_mini"] == "false" ) {
+						  }					  										    					  					  
 
-					  	echo "<li class=\"archive_list\"><a href=\"blog.php?id=" . $row["id"] . "\">" . $row["title"] . "</a> <span class=\"archive_list_timestamp\">(" . $dt->format('n/j/y') . ")</span></li>";
+						  //Handle a mini blog post.
+						  if ( $row["is_mini"] == "true" ) {					  	
+
+						  	//Print the blog post's body.
+						  	echo "<p>" . htmlspecialchars_decode($row["body"]) . "</p>";
+
+						  	//Print the blog post's timestamp/perm link.
+						  	echo "<p class=\"blog_post_timestamp\"><i class=\"fa fa-clock-o\" aria-hidden=\"true\" style=\"color: #F06768; font-size: 12px;\"></i> <a href=\"blog.php?id=" . $row["id"] . "\">Mini Post from " . $dt->format('F j, Y') . "</a></p>";
+
+						  }
+
+						  echo "</div>";
 
 						}
 
-					}							
+						//If this is not a perm link page, provide a link to the blog archive. Otherwise, provide a link back to the homepage.
+						if ( empty($id) ) {
 
-					echo "</ul>";
+							echo "<p class=\"navigation_link\"><i class=\"fa fa-arrow-circle-o-right\" style=\"color: #F06768; font-size: 14px;\" aria-hidden=\"true\"></i><a href=\"blog.php?archive=true\"> More blog posts...</a></p>";
 
-					echo "<p class=\"navigation_link\"><a href=\"blog.php\"><i class=\"fa fa-arrow-circle-o-left\" aria-hidden=\"true\" style=\"font-size: 14px;\"></i> Back to homepage...</a></p>";
+						} else {
 
-				}				
+							echo "<p class=\"navigation_link\"><a href=\"blog.php\"><i class=\"fa fa-arrow-circle-o-left\" aria-hidden=\"true\" style=\"font-size: 14px; \"></i> Back to homepage...</a></p>";
+
+						}
+
+				    break;
+
+				}	
 
 			?>
 			
